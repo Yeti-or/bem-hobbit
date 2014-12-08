2dcf20b11278a9b16d5b57d0cd932e172d328134
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2014-12-08 03:40:32 +0300

    Add simple deps reading.

--- walk.js
+++ walk.js
@@ -1,14 +1,61 @@
 
-var walk = require('bem-walk');
-var walker = walk(['common.blocks', 'desktop.blocks']);
+var walk = require('bem-walk'),
+    fs = require('fs');
+var walker = walk(['common.blocks', 'desktop.blocks', 'touch.blocks', 'touch-phone.blocks', 'touch-pad.blocks']);
 
-var bemo = [];
-walker.on('data', function (data) {
-    bemo.push(data);
-    if(data.tech === 'deps.js') {
-        console.log(data.id);
+var i = 0,j = 0;
+var blocks = {};
+function isBlock(bemObject) {
+    return bemObject.block && !bemObject.elem && !bemObject.modName;
+}
+walker.on('data', function (bemObject) {
+    i++;
+    if (!isBlock(bemObject)) return;
+    j++;
+
+    blocks[bemObject.block] = blocks[bemObject.block] || bemObject;
+    bemObject.must = [];
+    bemObject.should = [];
+    if (bemObject.tech === 'deps.js') {
+        fs.readFile(bemObject.path, 'utf8', function(err, data) {
+            if (err) throw err;
+
+            function analDep(deps, should) {
+                //!Array.isArray(deps) && console.log('No Array !!: ' + bemObject.id);
+                !Array.isArray(deps) && (deps = [deps]);
+                deps.forEach(function(dep) {
+                    //if (!isBlock(dep)) return;
+
+                    //console.log('Block : ' + bemObject.path + (should ? ' should ' : '') + ' deps : ' + dep.block);
+                    if (should) {
+                        blocks[bemObject.block].should.push(dep);
+                    } else {
+                        blocks[bemObject.block].must.push(dep);
+                    }
+                });
+
+            }
+
+
+
+            var deps = eval(data);
+            //TODO: Only tech:bemhml now
+            //TODO: Only blocks now
+            //!Array.isArray(deps) && console.log('FUCK No Array !!: ' + bemObject.id);
+            !Array.isArray(deps) && (deps = [deps]);
+            deps.forEach(function(dep) {
+                if (dep.tech) return;
+                dep.mustDeps && analDep(dep.mustDeps);
+                dep.shouldDeps && analDep(dep.shouldDeps, true);
+            });
+            var block = blocks[bemObject.block];
+            console.log('Block : ' + block.bem + ' deps : ' +       block.must.length);
+            console.log('Block : ' + block.bem + ' shouldDeps : ' + block.should.length);
+        });
     }
+    console.log(bemObject.path);
 }).on('end', function() {
-    console.log(bemo.length);
+    console.log(i);
+    console.log(j);
 });
 
