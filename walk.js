45567528195f5746889aa4b586b1c7db27d74958
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2015-04-11 15:18:35 +0300

    Meta info

--- walk.js
+++ walk.js
@@ -20,44 +20,59 @@ var fs = require('fs'),
     DepGraph = require('dependency-graph').DepGraph,
     graph = new DepGraph();
 
-function process(bemObject) {
-    //Add Node
-    var bem_id = bemNaming.stringify(bemObject.entity);
-
-    graph.hasNode(bem_id) || graph.addNode(bem_id);
-
-    if (bemObject.tech === 'deps.js') {
-        var data = fs.readFileSync(bemObject.path, 'utf8'),
-            deps = vm.runInThisContext(data);
-         
-        [].concat(deps).forEach(function(dep) {
-            //Add Connections
-            DEPS_TYPES.forEach(function(depType) {
-                [].concat(dep[depType]).forEach(function(dep) {
-                    if (dep) {
-                        // fixes for `deps-normalize`
-                        dep.block || (dep.block = bemObject.entity.block);
-                        dep.elems && !Array.isArray(dep.elems) && (dep.elems = [dep.elems]);
-                        typeof dep.mods === 'object' && Object.keys(dep.mods).forEach(function (mod) {
-                            typeof dep.mods[mod] === 'boolean' && (dep.mods[mod] = [dep.mods[mod]]);
-                        });
-
-                        normalize(dep).forEach(function(normDep) {
-                            var node = bemNaming.stringify(normDep);
-
-                            graph.hasNode(node) || graph.addNode(node);
-                            graph.addDependency(node, bem_id);
-                        });
-                    }
-                });
-            });
-        });
-    }
-
-}
 
 module.exports = function (levels, cb) {
     var walker = walk(levels);
+
+    var nodes = {};
+
+    function process(data) {
+        //Add Node
+        var target = bemNaming.stringify(data.entity);
+
+        graph.hasNode(target) || graph.addNode(target);
+
+        var t;
+        if (!(t = nodes[target])) {
+            //console.log('no ' + target);
+            nodes[target] = {id: target, label: target, levels:[data.level], techs: [data.tech], scope: data.entity.block };
+        } else {
+            //console.log('yes ' = target);
+
+            t.levels.push(data.level);
+            t.techs.push(data.tech);
+        } 
+
+        if (data.tech === 'deps.js') {
+            var depsData = fs.readFileSync(data.path, 'utf8'),
+                deps = vm.runInThisContext(depsData);
+             
+            [].concat(deps).forEach(function(dep) {
+                //Add Connections
+                DEPS_TYPES.forEach(function(depType) {
+                    [].concat(dep[depType]).forEach(function(dep) {
+                        if (dep) {
+                            // fixes for `deps-normalize`
+                            dep.block || (dep.block = data.entity.block);
+                            dep.elems && !Array.isArray(dep.elems) && (dep.elems = [dep.elems]);
+                            typeof dep.mods === 'object' && Object.keys(dep.mods).forEach(function (mod) {
+                                typeof dep.mods[mod] === 'boolean' && (dep.mods[mod] = [dep.mods[mod]]);
+                            });
+
+                            normalize(dep).forEach(function(normDep) {
+                                var node = bemNaming.stringify(normDep);
+
+                                graph.hasNode(node) || graph.addNode(node);
+                                graph.addDependency(node, target);
+                            });
+                        }
+                    });
+                });
+            });
+        }
+
+    }
+    
 
     walker
         .on('error', function(error) {
@@ -65,11 +80,10 @@ module.exports = function (levels, cb) {
         })
         .on('data', process)
         .on('end', function() {
-            var nodes = [],
-                edges = [];
+            var edges = [];
 
             Object.keys(graph.nodes).forEach(function(target) {
-                nodes.push({id: target, label: target});
+                //nodes.push({id: target, label: target});
                 graph.incomingEdges[target].forEach(function(dep) {
                     edges.push({ from: target, to: dep });
                 });
